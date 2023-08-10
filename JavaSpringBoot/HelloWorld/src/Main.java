ad596162786b4a96cc914a99aa5402239d7af705
Marc Kreistian Villegas <mlvillegas@ucdavis.edu>
2023-08-10 14:55:15 -0700

    First Commit

---
+++ JavaSpringBoot/HelloWorld/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static int Multiply(int x, int y){
+        int answer = x*y;
+        return answer;
+    }
+
+    
+    public static void main(String[] args) {
+        int x = 5;
+        int y=5;
+
+        int answer = Multiply(x,y);
+        System.out.println("Hello world!");
+        System.out.println(answer);
+
+    }
+}
